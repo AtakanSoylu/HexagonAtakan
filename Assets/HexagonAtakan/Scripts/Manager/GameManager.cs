f2df370bfbdc7e0d4b631f22d7e3853e76b2b539
AtakanSoylu <59143350+AtakanSoylu@users.noreply.github.com>
2021-06-21 18:53:49 +0300

    +add : input class for game manager

--- Assets/HexagonAtakan/Scripts/Manager/GameManager.cs
+++ Assets/HexagonAtakan/Scripts/Manager/GameManager.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using UnityEngine;
+using HexagonAtakan.PlayerInput;
+
 
 namespace HexagonAtakan.Manager
 {
     public class GameManager : MonoBehaviour
     {
         [SerializeField] private GridManager _gridManager;
+        [SerializeField] private InputData _inputData;
 
         private void Start()
         {
             _gridManager.InitializeHexagons();
         }
-        
-        
+
+        private void Update()
+        {
+            
+        }
 
 
     }
