0ddd9a493c117edefbcc026664402c7dfcf208bc
AtakanSoylu <59143350+AtakanSoylu@users.noreply.github.com>
2021-06-21 13:43:19 +0300

    +add : hexagon base mono

---
+++ Assets/HexagonAtakan/Scripts/Hexagon/HexagonBase.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace HexagonAtakan.Hexagon
+{
+    public class HexagonBase : MonoBehaviour
+    {
+        
+    }
+}
