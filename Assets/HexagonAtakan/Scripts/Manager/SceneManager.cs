7949ea8aae0a5160bd4f901bbbdb93a6b738ba77
AtakanSoylu <59143350+AtakanSoylu@users.noreply.github.com>
2021-06-26 10:53:50 +0300

    +change destroy time

--- Assets/HexagonAtakan/Scripts/Manager/SceneManager.cs
+++ Assets/HexagonAtakan/Scripts/Manager/SceneManager.cs
@@ -24,7 +24,7 @@ namespace HexagonAtakan.Manager
             {
                 _searchManager.SearchSceneHexagonGroup();
             }
-            Destroy(gameObject,3);
+            Destroy(gameObject,2);
         }
     }
 }
