451c7c18281a029e73eb57aea3930ee0bd8b1846
AtakanSoylu <59143350+AtakanSoylu@users.noreply.github.com>
2021-06-26 10:48:56 +0300

    +create : scene manager for testing

---
+++ Assets/HexagonAtakan/Scripts/Manager/SceneManager.cs
@@ -0,0 +1,30 @@
+﻿using HexagonAtakan.Hexagon;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HexagonAtakan.Manager
+{
+    public class SceneManager : MonoBehaviour
+    {
+
+        [SerializeField] private SearchManager _searchManager;
+        [SerializeField] private GridManager _gridManager;
+
+
+        private void LateUpdate()
+        {
+            StartScene();
+        }
+
+        //Mini hack (adjustment) :(
+        public void StartScene()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                _searchManager.SearchSceneHexagonGroup();
+            }
+            Destroy(gameObject,3);
+        }
+    }
+}
